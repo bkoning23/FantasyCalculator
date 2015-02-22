018b49e6e6925bcddc0a5a10073c96b9741724c4
bkoning23 <koningb@mail.gvsu.edu>
2015-02-22 14:29:09 -0500

    App functions, currently calculates KDA and CS. Poor visual design.

--- app/src/main/java/brendankoning/fantasycalculator/MainActivity.java
+++ app/src/main/java/brendankoning/fantasycalculator/MainActivity.java
@@ -1,17 +1,50 @@
 package brendankoning.fantasycalculator;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private TextView kills, assists, deaths, creep_score, points;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView.OnEditorActionListener listener = new TextView.OnEditorActionListener(){
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                calculate();
+                return true;
+            }
+        };
+
+
+
+        this.findViewById(R.id.calculateButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate();
+            }
+        });
+
+
+
+        kills = (TextView) findViewById(R.id.killsInput);
+        assists = (TextView) findViewById(R.id.assistsInput);
+        deaths = (TextView) findViewById(R.id.deathsInput);
+        points = (TextView) findViewById(R.id.points);
+        creep_score = (TextView) findViewById(R.id.csInput);
+        creep_score.setOnEditorActionListener(listener);
     }
 
 
@@ -36,4 +69,30 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+    public void calculate(){
+        InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(new View(this).getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+        double killCount, assistsCount, deathCount, creepCount, pointCount;
+        String totalPoints;
+
+        killCount = Double.parseDouble(kills.getText().toString());
+        assistsCount = Double.parseDouble(assists.getText().toString());
+        deathCount = Double.parseDouble(deaths.getText().toString());
+        creepCount = Double.parseDouble(creep_score.getText().toString());
+
+        pointCount = (killCount * 2) + (assistsCount * 1.5) - (deathCount*0.5) + (creepCount*0.01);
+
+        pointCount = Math.round(pointCount * 100);
+        pointCount = pointCount/100;
+
+        totalPoints = String.valueOf(pointCount);
+
+        points.setText(totalPoints);
+    }
+
+
 }
